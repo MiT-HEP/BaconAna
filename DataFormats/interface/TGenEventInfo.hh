7f4a64b1f96747e15f22761995eab042f0123449
Jay <jlawhorn@mit.edu>
2015-03-23 08:19:38 +0100

    save pdf info

--- DataFormats/interface/TGenEventInfo.hh
+++ DataFormats/interface/TGenEventInfo.hh
@@ -9,16 +9,18 @@ namespace baconhep
   {
   public:
     TGenEventInfo():
-      id_1(0),  id_2(0),
-      x_1(0),   x_2(0),
+      id_1(0),   id_2(0),
+      x_1(0),    x_2(0),
+      xPDF_1(0), xPDF_2(0),
       scalePDF(0),
       weight(0)
     {}
     ~TGenEventInfo(){}
     
     int   id_1,  id_2;   // parton flavor PDG ID
-    float x_1,   x_2;    // parton momentum fraction
-    float scalePDF;      // Q-scale used for PDF evaluation
+    double x_1,   x_2;    // parton momentum fraction
+    double xPDF_1,xPDF_2; // parton xF value
+    double scalePDF;      // Q-scale used for PDF evaluation
     float weight;        // event weight
     
     ClassDef(TGenEventInfo,1)
