6cb53196885f9887f8b8c479032a56501bacb710
Jay <jlawhorn@mit.edu>
2015-06-01 10:07:11 +0200

    add vertex info

--- DataFormats/interface/TGenParticle.hh
+++ DataFormats/interface/TGenParticle.hh
@@ -7,17 +7,19 @@ namespace baconhep
 {
   class TGenParticle : public TObject
   {
-    public:
-      TGenParticle():
-	parent(-1), pdgId(0),status(0),
-	pt(0), eta(0), phi(0), mass(0), y(0)
-      {}
-      ~TGenParticle(){}
-
-      int   parent;
-      int   pdgId;
-      int   status;
-      float pt, eta, phi, mass, y;
+  public:
+    TGenParticle():
+      parent(-1), pdgId(0),status(0),
+      pt(0), eta(0), phi(0), mass(0), y(0),
+      decx(0), decy(0), decz(0)
+    {}
+    ~TGenParticle(){}
+    
+    int   parent;
+    int   pdgId;
+    int   status;
+    float pt, eta, phi, mass, y;
+    float decx, decy, decz;
 
     ClassDef(TGenParticle,1)
   };
